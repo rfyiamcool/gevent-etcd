341d038a699b2f9e14eb3d93cd0100907ee6af2e
芮峰云 <rfyiamcool@163.com>
2015-10-11 10:29:27 +0800

    pypi 1.0

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+        name = "gevent_etcd",
+        version = "1.0",
+        author = "ruifengyun",
+        author_email = "rfyiamcool@163.com",
+        description = "a simple gevent requests etcd python client",
+        license = "MIT",
+        keywords = ["gevent etcd","fengyun"],
+        url = "https://github.com/rfyiamcool",
+        long_description = read('README.md'),
+        packages=['gevent_etcd'],
+        install_requires=['grequests','requests'],
+        classifiers = [
+             'Development Status :: 2 - Pre-Alpha',
+             'Intended Audience :: Developers',
+             'License :: OSI Approved :: MIT License',
+             'Programming Language :: Python :: 2.7',
+             'Programming Language :: Python :: 3.0',
+             'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+)
+
