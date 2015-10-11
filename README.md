f8e912b46e985ccc7a8918f488cd10142c44cdcc
芮峰云 <rfyiamcool@163.com>
2015-10-11 10:55:59 +0800

    pypi version 2.0

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 使用gevent requests封装的一个python client模块
 
+### ToDOList
+
+1. complement write function 
+
+2. fix stream watch bug
+
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
         name = "gevent_etcd",
-        version = "1.0",
+        version = "2.0",
         author = "ruifengyun",
         author_email = "rfyiamcool@163.com",
         description = "a simple gevent requests etcd python client",
